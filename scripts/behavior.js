a5e22e945769e0a6ecec4a8428ea6426e164f2b5
aclaudiadavid <aclaudia.david2000@gmail.com>
2021-10-30 00:34:28 +0100

    map is up

--- scripts/behavior.js
+++ scripts/behavior.js
@@ -1,37 +1,45 @@
 var map = "/data/ContinenteConcelhos.json"; //world view
+var tvotes = "/data/resultados_eleicoes.json";
 
 var map2;
+var votes;
 
-var width = 1000;
-var height = 400;
+var width = 500;
+var height = 600;
 
 margin = { top: 20, right: 20, bottom: 20, left:40 };
 
 Promise.all([d3.json(map)]).then(function (map) {
     map2 = map;
+    console.log(map2)
     generate_map();
+    //generate_stacked();
     addZoom();
   });
 
 function generate_map() {
   var projection = d3
     .geoMercator()
-    .scale(height / 2)
-    .rotate([0, 0])
-    .center([0, 0])
-    .translate([width / 2, height / 2]);
+    .scale(6000)
+    .rotate([5, 5000])
+    //.translate([width / 2, height / 2]);
 
   map2 = map2[0];
-  console.log(map2);
 
   var geog = d3.geoPath().projection(projection);
 
-  d3.select("#map")
+  console.log(map2);
+
+  svg = d3.select("#map")
     .append("svg")
     .attr("width", width)
     .attr("height", height)
+
+  svg.append("g")
     .selectAll("path")
-    .data(topojson.feature(map2, map2.objects.ContinenteConcelhos).features)
+    .data(topojson.feature(map2, map2.objects.Concelhos).features)
+    //.data(topojson.feature(map2, map2.objects.concelhos).features)
+    //.data(map2.features)
     .join("path")
     .attr("class", "Concelho")
     .attr("d", geog)
@@ -44,6 +52,18 @@ function generate_map() {
     .text(function (d) {
       return d.properties.name;
     })*/;
+}
+
+function generate_stacked() {
+  votes = d3.json(tvotes);
+  var svg = d3.select("#stacked")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  
+  console.log(votes);
 }
 
 function handleMouseOver(event, d) {
