a21c27a7399df219a3a5eb237ba93c84a404dc9b
extreme33-ux <miguel@LAPTOP-V7AGU8Q3.localdomain>
2021-10-31 14:38:14 +0000

    Stack and Line Chart

--- scripts/behavior.js
+++ scripts/behavior.js
@@ -9,12 +9,14 @@ var height = 600;
 
 margin = { top: 20, right: 20, bottom: 20, left:40 };
 
-Promise.all([d3.json(map)]).then(function (map) {
-    map2 = map;
-    console.log(map2)
-    generate_map();
+Promise.all([d3.json(map), d3.json(tvotes)]).then(function (d) {
+    map2 = d[0];
+    votes = d[1];
+    //console.log(votes);
+    //generate_map();
     //generate_stacked();
-    addZoom();
+    generate_line_chart();
+    //addZoom();
   });
 
 function generate_map() {
@@ -24,11 +26,7 @@ function generate_map() {
     .rotate([5, 5000])
     //.translate([width / 2, height / 2]);
 
-  map2 = map2[0];
-
   var geog = d3.geoPath().projection(projection);
-
-  console.log(map2);
 
   svg = d3.select("#map")
     .append("svg")
@@ -53,15 +51,124 @@ function generate_map() {
 }
 
 function generate_stacked() {
-  votes = d3.json(tvotes);
+
+  //var concelhos = Object.keys(votes);
+  //console.log(concelhos);
+  var anos_eleicoes = Object.keys(votes["Portugal"]);
+  var votos_portugal = votes["Portugal"];
+  console.log(votos_portugal);
+
   var svg = d3.select("#stacked")
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  // Add X axis
+  var x = d3.scaleBand()
+      .domain(anos_eleicoes)
+      .range([0, width])
+      .padding([0.2])
+  svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x).tickSizeOuter(0));
   
-  console.log(votes);
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain([0, 100])
+    .range([ height, 0 ]);
+  svg.append("g")
+    .call(d3.axisLeft(y));
+
+  // color palette = one color per subgroup
+  var color = d3.scaleOrdinal()
+    .domain(["votos", "abstencao"])
+    .range(['#e41a1c','#377eb8']);
+
+  var stackedData = d3.stack()
+    .keys(["votos", "abstencao"]);
+  
+  var dt = stackedData(votos_portugal);
+
+  
+}
+
+function generate_line_chart(){
+
+  width = 500;
+
+  height = 400;
+
+  margin = { top: 20, right: 20, bottom :20, left: 40};
+
+
+  var anos_eleicoes = Object.keys(votes["Cascais"]);
+  var votos_concelho = Object.values(votes["Cascais"]);
+
+  /*votos_PS = votos_concelho.filter(function(d) {
+               return d.PS;
+  });
+  console.log(votos_PS);*/
+
+  line = d3
+        .line()
+        .defined(function (d) {
+          console.log(d);
+          return d.PS > 0;
+        })
+        .x((d) => d.total)
+        .y((d) => d.PS);
+
+  
+  var svg = d3.select("#lineChart")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    
+    x = d3
+        .scaleLinear()
+        .domain([1993,2017])
+        .nice()
+        .range([margin.left, width - margin.right]);
+    
+    y = d3
+        .scaleLinear()
+        .domain([0,100000])
+        .range([height -margin.bottom, margin.top]);
+
+    xAxis = (g) =>
+        g.attr("transform", `translate(0,${height - margin.bottom})`)
+            .call(
+                d3
+                    .axisBottom(x)
+                    .tickFormat((x) => x)
+                    .tickSizeOuter(0)
+            );
+
+    yAxis = (g) =>
+        g
+            .attr("transform", `translate(${margin.left},0)`)
+            .call(d3
+                    .axisLeft(y)
+                    .tickFormat((x) => x));
+
+    svg.append("g").call(xAxis);
+
+    svg.append("g").call(yAxis); 
+
+
+  svg.append("path")
+     .datum(votos_concelho)
+     .attr("fill", "none")
+     .attr("stroke", "steelblue")
+     .attr("stroke-width", 1.5)
+     .attr("stroke-linejoin", "round")
+     .attr("stroke-linecap", "round")
+     .attr("d", line);
+
 }
 
 function handleMouseOver(event, d) {
